ee97ee9880729b72b779c98b8bab19af92e32714
Wikan haydarrahman <wikanhaydar23peb@gmail.com>
2025-07-12 17:23:25 +0700

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -5,24 +5,20 @@ from passlib.context import CryptContext
 from jose import jwt
 from . import config
 
-# Konteks untuk hashing password
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Memverifikasi password biasa dengan hash-nya."""
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
-    """Membuat hash dari password."""
     return pwd_context.hash(password)
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    """Membuat JWT access token."""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, config.APP_SECRET_KEY, algorithm=config.ALGORITHM)
     return encoded_jwt
